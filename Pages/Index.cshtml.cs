cde8ac2726ec9f8b38b79e67d533fa67f032c579
Herve Dzeudjouo <dzeudjouo@gmail.com>
2023-09-03 12:58:07 -0600

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Services;
+
+namespace MyWebApp.Pages
+{ 
+    public class IndexModel : PageModel
+    {
+        public List<Product>? Products; 
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products= productService.GetProducts();
+
+        }
+    }
+}
